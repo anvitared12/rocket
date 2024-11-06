83f04b3d1601d5a6182b3a93b3740dddc614d533
anvitared12 <anvitasreddy@gmail.com>
2024-11-06 14:05:25 +0530

    commit

---
+++ Arraylist.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Arraylist {
+    //write a java prgrm to insert your friends names
+    /* 
+    public static void main(String[] args) {
+        ArrayList<String> friends=new ArrayList<String>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your friends names:");
+        while(true){   //keeps on running until u write exit
+            String name=sc.nextLine();
+            // if(name.equals("exit")){
+            //     break;
+            // }
+            // else{
+            //     friends.add(name);
+            // }
+            if(!name.equals("exit")){
+                friends.add(name);
+
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println(friends);
+        int len=friends.size();
+        friends.remove(len-1);
+        System.out.println(friends);// remove last elements
+        System.out.println("i had little fight with" + friends.get(0));
+       
+        System.out.println("i need 10lc");
+        System.out.println("now my friends");
+        friends.clear();
+        System.out.println(friends);
+
+    }
+        */
+    //[raj,karan,raj,gopal,som,karan]
+    //remove duplicates
+    //maximum repeated name
+    //minimum repeated
+
+    //implementing hash map
+
+    public static void main(String[] args) {
+        
+    }
+}
