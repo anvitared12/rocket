9b93968245b7a30437218bb366995f020be72b8d
anvitared12 <anvitasreddy@gmail.com>
2025-02-19 17:52:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')  # Explicitly define template folder
 
 @app.route('/')
 def home():
@@ -8,3 +8,4 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
